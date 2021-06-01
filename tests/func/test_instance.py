f11ca35a501a830f967b47c65265e59bae7b59f9
Denis Laxalde <denis.laxalde@dalibo.com>
2021-06-01 14:03:35 +0200

    Move test_configure() as a unit test
    There's no need for a real instance for this test.

--- tests/func/test_instance.py
+++ tests/func/test_instance.py
@@ -2,7 +2,6 @@ import subprocess
 
 import psycopg2
 import pytest
-from pgtoolkit.conf import parse as parse_pgconf
 from pgtoolkit.ctl import Status
 
 from pglift import instance as instance_mod
@@ -68,94 +67,6 @@ def test_init(ctx, instance_initialized):
     i = Instance("test", "9.6", settings=ctx.settings)
     with pytest.raises(EnvironmentError, match="pg_ctl executable not found"):
         instance_mod.init(ctx, i)
-
-
-@pytest.fixture
-def ctx_nohook(ctx):
-    unregistered = ctx.pm.unregister_all()
-    try:
-        yield ctx
-    finally:
-        for plugin in unregistered:
-            ctx.pm.register(plugin)
-
-
-def test_configure(ctx_nohook):
-    ctx = ctx_nohook
-    i = Instance.default_version("test-configure", ctx=ctx)
-    configdir = i.datadir
-    configdir.mkdir(parents=True)
-    postgresql_conf = i.datadir / "postgresql.conf"
-    with postgresql_conf.open("w") as f:
-        f.write("bonjour_name = 'test'\n")
-    initial_content = postgresql_conf.read_text()
-
-    changes = instance_mod.configure(ctx, i, port=5433, max_connections=100)
-    assert changes == {
-        "cluster_name": (None, "test-configure"),
-        "max_connections": (None, 100),
-        "port": (None, 5433),
-    }
-    with postgresql_conf.open() as f:
-        line1 = f.readline().strip()
-    assert line1 == "include_dir = 'conf.pglift.d'"
-
-    configfpath = configdir / "conf.pglift.d" / "user.conf"
-    lines = configfpath.read_text().splitlines()
-    assert "port = 5433" in lines
-    assert "cluster_name = 'test-configure'" in lines
-
-    with postgresql_conf.open() as f:
-        config = parse_pgconf(f)
-    assert config.port == 5433
-    assert config.bonjour_name == "test"
-    assert config.cluster_name == "test-configure"
-
-    changes = instance_mod.configure(ctx, i, listen_address="*", ssl=True)
-    assert changes == {
-        "listen_address": (None, "*"),
-        "max_connections": (100, None),
-        "port": (5433, None),
-        "ssl": (None, True),
-    }
-    # Same configuration, no change.
-    mtime_before = postgresql_conf.stat().st_mtime, configfpath.stat().st_mtime
-    changes = instance_mod.configure(ctx, i, listen_address="*", ssl=True)
-    assert changes == {}
-    mtime_after = postgresql_conf.stat().st_mtime, configfpath.stat().st_mtime
-    assert mtime_before == mtime_after
-
-    instance_mod.revert_configure(ctx, i)
-    assert postgresql_conf.read_text() == initial_content
-    assert not configfpath.exists()
-
-    instance_mod.configure(ctx, i, ssl=True)
-    lines = configfpath.read_text().splitlines()
-    assert "ssl = on" in lines
-    assert (configdir / "server.crt").exists()
-    assert (configdir / "server.key").exists()
-
-    instance_mod.revert_configure(ctx, i, ssl=True)
-    assert not (configdir / "server.crt").exists()
-    assert not (configdir / "server.key").exists()
-
-    ssl = (cert_file, key_file) = (i.datadir / "c.crt", i.datadir / "k.key")
-    for fpath in ssl:
-        fpath.touch()
-    changes = instance_mod.configure(ctx, i, ssl=ssl)
-    assert changes == {
-        "cluster_name": (None, i.name),
-        "ssl": (None, True),
-        "ssl_cert_file": (None, cert_file),
-        "ssl_key_file": (None, key_file),
-    }
-    lines = configfpath.read_text().splitlines()
-    assert "ssl = on" in lines
-    assert f"ssl_cert_file = {i.datadir / 'c.crt'}" in lines
-    assert f"ssl_key_file = {i.datadir / 'k.key'}" in lines
-    instance_mod.revert_configure(ctx, i, ssl=ssl)
-    for fpath in ssl:
-        assert fpath.exists()
 
 
 def test_configure_auth(ctx, instance_auth_configured):
