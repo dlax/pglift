c7d918983e2d679ea45df5db066d10de3e75c101
Denis Laxalde <denis.laxalde@dalibo.com>
2021-06-09 09:57:20 +0200

    Add mypy to 'typing' extra requirements

--- setup.py
+++ setup.py
@@ -7,6 +7,7 @@ here = pathlib.Path(__file__).parent.resolve()
 long_description = (here / "README.md").read_text(encoding="utf-8")
 
 extras_typing = [
+    "mypy",
     "types-PyYAML",
     "types-requests",
 ]
