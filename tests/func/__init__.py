3e9da1a458a79315d5bc5f8c438374f1debd07df
Denis Laxalde <denis.laxalde@dalibo.com>
2021-06-10 17:35:29 +0200

    Add an execute() test helper function

--- tests/func/__init__.py
+++ tests/func/__init__.py
@@ -1,8 +1,11 @@
 from contextlib import contextmanager
 from pathlib import Path
-from typing import Any, Iterator, Optional
+from typing import Any, Iterator, Optional, Tuple, overload
 
-from pglift import instance
+from typing_extensions import Literal
+
+from pglift import db
+from pglift import instance as instance_mod
 from pglift.ctx import BaseContext
 from pglift.model import Instance
 
@@ -22,7 +25,7 @@ def configure_instance(
             port = config.port  # type: ignore[assignment]
         if not socket_path:
             socket_path = Path(config.unix_socket_directories)  # type: ignore[arg-type]
-    instance.configure(
+    instance_mod.configure(
         ctx, i, port=port, unix_socket_directories=str(socket_path), **confitems
     )
 
@@ -38,3 +41,35 @@ def reconfigure_instance(ctx: BaseContext, i: Instance, *, port: int) -> Iterato
         yield
     finally:
         configure_instance(ctx, i, port=initial_port)  # type: ignore[arg-type]
+
+
+@overload
+def execute(
+    ctx: BaseContext, instance: Instance, query: str, fetch: Literal[False]
+) -> None:
+    ...
+
+
+@overload
+def execute(
+    ctx: BaseContext, instance: Instance, query: str, fetch: Literal[True]
+) -> Tuple[Any, ...]:
+    ...
+
+
+@overload
+def execute(ctx: BaseContext, instance: Instance, query: str) -> Tuple[Any, ...]:
+    ...
+
+
+def execute(
+    ctx: BaseContext, instance: Instance, query: str, fetch: bool = True
+) -> Optional[Tuple[Any, ...]]:
+    with instance_mod.running(ctx, instance):
+        with db.connect(instance, ctx.settings.postgresql.surole) as conn:
+            with conn.cursor() as cur:
+                cur.execute(query)
+                conn.commit()
+                if fetch:
+                    return cur.fetchall()  # type: ignore[no-any-return]
+        return None
