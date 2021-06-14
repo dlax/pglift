c8bdea7f82d34975080961d6f7f06899e29b9f87
Denis Laxalde <denis.laxalde@dalibo.com>
2021-06-14 09:28:05 +0200

    Always pass 'obj=ctx' in CLI tests
    Per consistency, although this is not always required, this would
    prevent undesired behaviors like command operating on user settings and
    writing outside temporary directories.

--- tests/unit/test_cli.py
+++ tests/unit/test_cli.py
@@ -17,13 +17,13 @@ def runner():
     return CliRunner()
 
 
-def test_cli(runner):
-    result = runner.invoke(cli)
+def test_cli(runner, ctx):
+    result = runner.invoke(cli, obj=ctx)
     assert result.exit_code == 0
 
 
 def test_instance_apply(tmp_path, runner, ctx):
-    result = runner.invoke(cli, ["instance", "apply"])
+    result = runner.invoke(cli, ["instance", "apply"], obj=ctx)
     assert result.exit_code == 2
     assert "Missing option '-f'" in result.output
 
@@ -37,49 +37,49 @@ def test_instance_apply(tmp_path, runner, ctx):
     assert isinstance(mock_method.call_args[0][1], manifest_mod.Instance)
 
 
-def test_instance_schema(runner):
-    result = runner.invoke(cli, ["instance", "schema"])
+def test_instance_schema(runner, ctx):
+    result = runner.invoke(cli, ["instance", "schema"], obj=ctx)
     schema = json.loads(result.output)
     assert schema["title"] == "Instance"
     assert schema["description"] == "PostgreSQL instance"
 
 
-def test_instance_describe(runner):
-    result = runner.invoke(cli, ["instance", "describe"])
+def test_instance_describe(runner, ctx):
+    result = runner.invoke(cli, ["instance", "describe"], obj=ctx)
     assert result.exit_code == 2
     assert "Missing argument 'NAME'" in result.output
 
     instance = manifest_mod.Instance(name="test")
     with patch.object(instance_mod, "describe", return_value=instance) as mock_method:
-        result = runner.invoke(cli, ["instance", "describe", "test"])
+        result = runner.invoke(cli, ["instance", "describe", "test"], obj=ctx)
     mock_method.assert_called_once()
     assert isinstance(mock_method.call_args[0][0], Context)
     assert "name: test" in result.output
 
 
-def test_instance_drop(runner):
-    result = runner.invoke(cli, ["instance", "drop"])
+def test_instance_drop(runner, ctx):
+    result = runner.invoke(cli, ["instance", "drop"], obj=ctx)
     assert result.exit_code == 2
     assert "Missing argument 'NAME'" in result.output
 
     with patch.object(instance_mod, "drop") as mock_method:
-        result = runner.invoke(cli, ["instance", "drop", "test"])
+        result = runner.invoke(cli, ["instance", "drop", "test"], obj=ctx)
     mock_method.assert_called_once()
     assert isinstance(mock_method.call_args[0][0], Context)
 
 
-def test_backup_instance(runner):
+def test_backup_instance(runner, ctx):
     patch_backup = patch.object(pgbackrest, "backup")
     patch_expire = patch.object(pgbackrest, "expire")
     with patch_backup as backup, patch_expire as expire:
-        result = runner.invoke(cli, ["backup-instance", "test", "--type=diff"])
+        result = runner.invoke(cli, ["backup-instance", "test", "--type=diff"], obj=ctx)
     assert result.exit_code == 0, result.stdout
     assert backup.call_count == 1
     assert backup.call_args[1] == {"type": pgbackrest.BackupType("diff")}
     assert not expire.called
 
     with patch_backup as backup, patch_expire as expire:
-        result = runner.invoke(cli, ["backup-instance", "test", "--purge"])
+        result = runner.invoke(cli, ["backup-instance", "test", "--purge"], obj=ctx)
     assert result.exit_code == 0, result.stdout
     assert backup.called
     assert expire.called
