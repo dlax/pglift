8a0ceadf222cb423544399810a245f7bcd12d2e1
Denis Laxalde <denis.laxalde@dalibo.com>
2021-08-30 10:49:06 +0200

    Split pgbackrest functional test

--- tests/func/test_20_pgbackrest.py
+++ tests/func/test_20_pgbackrest.py
@@ -15,19 +15,44 @@ pytestmark = pytest.mark.skipif(
 )
 
 
-def test(ctx, installed, instance, tmp_path, tmp_port_factory):
+@pytest.fixture
+def directory(ctx, instance):
+    pgbackrest_settings = ctx.settings.pgbackrest
+    return Path(str(pgbackrest_settings.directory).format(instance=instance))
+
+
+def test_configure(ctx, installed, instance, tmp_path, tmp_port_factory, directory):
     instance_config = instance.config()
     assert instance_config
     instance_port = instance_config.port
     pgbackrest_settings = ctx.settings.pgbackrest
 
     configpath = Path(str(pgbackrest_settings.configpath).format(instance=instance))
-    directory = Path(str(pgbackrest_settings.directory).format(instance=instance))
     assert configpath.exists()
     lines = configpath.read_text().splitlines()
     assert f"pg1-port = {instance_port}" in lines
     assert directory.exists()
 
+    # Calling setup an other time doesn't overwrite configuration
+    configdir = instance.datadir
+    confd = conf_info(configdir)[0]
+    pgconfigfile = confd / "pgbackrest.conf"
+    mtime_before = configpath.stat().st_mtime, pgconfigfile.stat().st_mtime
+    pgbackrest.setup(ctx, instance)
+    mtime_after = configpath.stat().st_mtime, pgconfigfile.stat().st_mtime
+    assert mtime_before == mtime_after
+
+    # If instance's configuration changes, pgbackrest configuration is
+    # updated.
+    config_before = configpath.read_text()
+    new_port = next(tmp_port_factory)
+    with reconfigure_instance(ctx, instance, port=new_port):
+        config_after = configpath.read_text()
+        assert config_after != config_before
+        assert f"pg1-port = {new_port}" in config_after.splitlines()
+
+
+def test_backup(ctx, instance, directory):
     latest_backup = (
         directory / "backup" / f"{instance.version}-{instance.name}" / "latest"
     )
@@ -55,21 +80,3 @@ def test(ctx, installed, instance, tmp_path, tmp_port_factory):
     assert backup1.type == "full"
     assert backup1.databases == "postgres"
     assert backup1.datetime.replace(tzinfo=None) > before
-
-    # Calling setup an other time doesn't overwrite configuration
-    configdir = instance.datadir
-    confd = conf_info(configdir)[0]
-    pgconfigfile = confd / "pgbackrest.conf"
-    mtime_before = configpath.stat().st_mtime, pgconfigfile.stat().st_mtime
-    pgbackrest.setup(ctx, instance)
-    mtime_after = configpath.stat().st_mtime, pgconfigfile.stat().st_mtime
-    assert mtime_before == mtime_after
-
-    # If instance's configuration changes, pgbackrest configuration is
-    # updated.
-    config_before = configpath.read_text()
-    new_port = next(tmp_port_factory)
-    with reconfigure_instance(ctx, instance, port=new_port):
-        config_after = configpath.read_text()
-        assert config_after != config_before
-        assert f"pg1-port = {new_port}" in config_after.splitlines()
