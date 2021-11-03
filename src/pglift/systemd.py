5ea4e4ef0bdc4697f33b7d8cbf968027ce974666
Denis Laxalde <denis.laxalde@dalibo.com>
2021-11-03 10:54:19 +0100

    Show 100 lines when running systemctl status
    The default 10 lines might not be enough when a traceback is included.

--- src/pglift/systemd.py
+++ src/pglift/systemd.py
@@ -76,7 +76,9 @@ def log_status(fn: F) -> F:
 
 
 def status(ctx: BaseContext, unit: str) -> str:
-    proc = ctx.run(["systemctl", "--user", "--full", "status", unit], check=False)
+    proc = ctx.run(
+        ["systemctl", "--user", "--full", "--lines=100", "status", unit], check=False
+    )
     # https://www.freedesktop.org/software/systemd/man/systemctl.html#Exit%20status
     if proc.returncode not in (0, 1, 2, 3, 4):
         raise exceptions.CommandError(
