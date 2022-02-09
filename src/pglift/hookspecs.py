10a3c25a00ca102f6c1f8995e80a405395b4243e
Denis Laxalde <denis.laxalde@dalibo.com>
2022-02-09 15:46:21 +0100

    Avoid local imports in hookspecs module
    This would trigger circular imports when we'll define the PluginManager
    at package-level next.

--- src/pglift/hookspecs.py
+++ src/pglift/hookspecs.py
@@ -1,45 +1,49 @@
+from typing import TYPE_CHECKING
+
 import pluggy
 from pgtoolkit.conf import Configuration
 
 from . import __name__ as pkgname
-from .ctx import BaseContext
-from .models import interface
-from .models.system import Instance
-from .types import ConfigChanges
+
+if TYPE_CHECKING:
+    from .ctx import BaseContext
+    from .models import interface
+    from .models.system import Instance
+    from .types import ConfigChanges
 
 hookspec = pluggy.HookspecMarker(pkgname)
 
 
 @hookspec  # type: ignore[misc]
-def install_systemd_unit_template(ctx: BaseContext, header: str = "") -> None:
+def install_systemd_unit_template(ctx: "BaseContext", header: str = "") -> None:
     """Install systemd unit templates."""
 
 
 @hookspec  # type: ignore[misc]
-def uninstall_systemd_unit_template(ctx: BaseContext) -> None:
+def uninstall_systemd_unit_template(ctx: "BaseContext") -> None:
     """Uninstall systemd unit templates."""
 
 
 @hookspec  # type: ignore[misc]
 def instance_configure(
-    ctx: BaseContext,
-    manifest: interface.Instance,
+    ctx: "BaseContext",
+    manifest: "interface.Instance",
     config: Configuration,
-    changes: ConfigChanges,
+    changes: "ConfigChanges",
 ) -> None:
     """Called when the PostgreSQL instance got (re-)configured."""
 
 
 @hookspec  # type: ignore[misc]
-def instance_drop(ctx: BaseContext, instance: Instance) -> None:
+def instance_drop(ctx: "BaseContext", instance: "Instance") -> None:
     """Called when the PostgreSQL instance got dropped."""
 
 
 @hookspec  # type: ignore[misc]
-def instance_start(ctx: BaseContext, instance: Instance) -> None:
+def instance_start(ctx: "BaseContext", instance: "Instance") -> None:
     """Called when the PostgreSQL instance got started."""
 
 
 @hookspec  # type: ignore[misc]
-def instance_stop(ctx: BaseContext, instance: Instance) -> None:
+def instance_stop(ctx: "BaseContext", instance: "Instance") -> None:
     """Called when the PostgreSQL instance got stopped."""
